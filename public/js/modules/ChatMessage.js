88de0d22ed9bb5ee5d93f5632d1a57690ca36c69
kchang00 <k_chang@fanshaweonline.ca>
2020-01-28 15:21:15 -0500

    initial files

---
+++ public/js/modules/ChatMessage.js
@@ -0,0 +1,6 @@
+// the export statement means that everything inside the curly braces 
+// will be made public when you import this file into another via the import statement
+
+export default {
+
+}
